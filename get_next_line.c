1c19d2142cc3fb6c43bb744e7715f8baf44b01c2
worldii <jongha2788@u.sogang.ac.kr>
2021-12-07 11:19:36 +0900

    sdas

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jongha2788 <jongha2788@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 18:26:30 by jonghapa          #+#    #+#             */
-/*   Updated: 2021/12/06 18:24:44 by jonghapa         ###   ########.fr       */
+/*   Updated: 2021/12/07 11:17:30 by jongha2788       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,27 +35,24 @@ char	*split_line(char **buf)
 	newlineidx = is_newline(*buf);
 	if (newlineidx == -1)
 	{
-		line = (char *) malloc(sizeof(char) * ft_strlen(*buf) + 1);
-		idx = -1;
-		while (++idx < ft_strlen(*buf))
-			line[idx] = (*buf)[idx];
-		line[idx] = 0;
+		// EOF 라는 뜻임. 
+		line = ft_strdup(*buf);
+		free(*buf);
+		*buf =NULL;
 	}
 	else
 	{
-		line = (char *) malloc(sizeof(char) * (newlineidx + 1));
+		line = (char *) malloc(newlineidx + 1);
 		idx = -1;
 		while (++idx < newlineidx)
-		{
 			line[idx] = (*buf)[idx];
-		}
 		line[idx] = 0;
 		temp = (char *)malloc(ft_strlen(*buf) - newlineidx + 1);
 		idx = -1;
 		while (++idx < ft_strlen(*buf) - newlineidx)
 			temp[idx] = (*buf)[newlineidx + idx + 1];
-		free(*buf);
 		temp[idx] = 0;
+		free(*buf);
 		*buf = temp ;
 	}
 	return (line);
@@ -65,6 +62,7 @@ char	*get_next_line(int fd)
 {
 	char		buf[BUFFER_SIZE + 1];
 	static char	*backup ;
+	char *newbackup;
 	int			read_size;
 	int			nextidx;
 
@@ -77,7 +75,9 @@ char	*get_next_line(int fd)
 	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[read_size] = 0;
-		backup = ft_strjoin(backup, buf);
+		newbackup = ft_strjoin(backup, buf);
+		free(backup);
+		backup = newbackup;
 		if ((nextidx = is_newline(backup)) != -1)
 			return (split_line(&backup));
 	}
--- main.c
+++ main.c
@@ -8,13 +8,17 @@ int main(void)
 	char *line = 0;
 	int ret;
 	int fd;
+	//system("leaks a.out");
 	fd = open("testfile2", O_RDONLY);
 	line = get_next_line(fd);
 	if (line !=0) printf("printf %s\n", line);
+	free(line);
 	line = get_next_line ( fd);
 	if (line != 0 ) printf("printf %s\n", line);
+	free(line);
 	line = get_next_line ( fd);
 	if (line != 0)printf("printf %s\n", line );
+	free(line);
 	return (0);
 }
 
